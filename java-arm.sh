2002a0d2b956e71c9b9b702029e163ea80a9ca11
Ismael Damião <53145011+ismaeldamiao@users.noreply.github.com>
2021-01-03 01:19:04 -0300

    Update java-arm.sh

--- java-arm.sh
+++ java-arm.sh
@@ -13,4 +13,4 @@ apt install -y ./openjdk-9-jre-headless_9.2017.8.20-1_arm.deb
 wget https://archive.org/download/openjdk-9-jre-headless_9.2017.8.20-1_x86_64/openjdk-9-jdk-headless_9.2017.8.20-1_arm.deb
 apt install -y ./openjdk-9-jdk-headless_9.2017.8.20-1_arm.deb
 
-rm openjdk-9-jdk-headless_9.2017.8.20-1_arm.deb
+rm openjdk-9-jre-headless_9.2017.8.20-1_arm.deb openjdk-9-jdk-headless_9.2017.8.20-1_arm.deb
